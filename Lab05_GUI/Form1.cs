e4c501e34b9887a171b0a11e52535b58c16a5a16
phucdang <dangthanhphuc2512@gmail.com>
2023-10-12 18:36:17 +0700

    test branck

--- Lab05_GUI/Form1.cs
+++ Lab05_GUI/Form1.cs
@@ -21,6 +21,8 @@ namespace Lab05_GUI
         public frmQuanLySinhVien()
         {
             InitializeComponent();
+            test
+
         }
 
         private void frmQuanLySinhVien_Load(object sender, EventArgs e)
